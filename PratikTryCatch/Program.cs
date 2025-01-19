545d743d315e6eb2e64bebc308e54be4ede67971
bburakbbasol <bburakbbasol@gmail.com>
2025-01-19 14:03:37 +0300

    Add project files.

---
+++ PratikTryCatch/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+internal class Program
+{
+	private static void Main(string[] args)
+
+	{
+		//Sonsuz döngü başlat
+		while (true)
+		{
+			//Hata vermesini beklediğin kodu yaz
+			try
+			{
+				Console.Write("Bir değer giriniz: ");
+				string deger = Console.ReadLine();
+				int sayi = Convert.ToInt32(deger);
+				Console.WriteLine("Giridiğiniz Sayının Karesi: " + (sayi * sayi));
+				//Hatasız şekilde çalışdığı zaman break kodu çalışacak ve while döngüsünden çıkmış olduk 
+				break;
+			}
+			//Hataları Yakala ve Mesaj ver
+			catch (FormatException )
+			{
+
+				Console.WriteLine("Lütfen sadece sayi giriniz");
+			}
+			catch (OverflowException )
+			{
+				Console.WriteLine("Çok büyük veya çok küçük sayı girişinde bulundunuz: ");
+			}
+
+
+
+
+		}
+		
+	}
+}
